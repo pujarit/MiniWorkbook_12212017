1ea26d5003f806ef0de18d336bf9c7ad6bbfa500
pujarit <ravitej.pujari@heidtman.com>
2018-01-03 01:11:02 +0530

    Update workbook.js

--- js/workbook.js
+++ js/workbook.js
@@ -1,6 +1,74 @@
 //var caseId = 'E1706170812';
 var caseId = 'N0003000069';
 
+function ShowLoader() {
+    waitingDialog.show();
+}
+
+function HideLoader() {
+    waitingDialog.hide();
+}
+
+function OnError(text)
+{
+	waitingDialog.show(text);
+	setTimeout(function () {waitingDialog.hide();}, 5000);
+}
+
+var waitingDialog = (function ($) {
+
+    // Creating modal dialog's DOM
+	var $dialog = $(
+		'<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+		'<div class="modal-dialog modal-m">' +
+		'<div class="modal-content">' +
+			'<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
+			'<div class="modal-body">' +
+				'<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+			'</div>' +
+		'</div></div></div>');
+
+	return {
+		/**
+		 * Opens our dialog
+		 * @param message Custom message
+		 * @param options Custom options:
+		 * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
+		 * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
+		 */
+		show: function (message, options) {
+			// Assigning defaults
+			var settings = $.extend({
+				dialogSize: 'm',
+				progressType: ''
+			}, options);
+			if (typeof message === 'undefined') {
+				message = 'Loading';
+			}
+			if (typeof options === 'undefined') {
+				options = {};
+			}
+			// Configuring dialog
+			$dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
+			$dialog.find('.progress-bar').attr('class', 'progress-bar');
+			if (settings.progressType) {
+				$dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
+			}
+			$dialog.find('h3').text(message);
+			// Opening dialog
+			$dialog.modal();
+		},
+		/**
+		 * Closes dialog
+		 */
+		hide: function () {
+			$dialog.modal('hide');
+		}
+	}
+
+})(jQuery);
+
+
 function getServiceUrl(datasource) {
 	var serviceURL = "https://mcr-cc.test.cognosante.cc:443";
 	//var serviceURL = "https://impl.cerrs-mcr.cms.gov";	
@@ -115,12 +183,18 @@ app.controller("workbookcontroller", function ($scope, Service) {
             if($scope.MIDAS.length < 1){
                 //console.log('NO DATA FOUND');
                 $scope.noMidasDataFlag = true;
+				OnError('NO DATA FOUND');
             }
+			else
+				HideLoader();
+			
         }, function (msg) {  			
 			if(msg.status > 1 || msg.status== -1){
 			$scope.MidasErrorFlag = true;	
 			$scope.ShowLoadingMsg = false;
-			$scope.MidasErrorMsg = "ERROR RETRIEVING MIDAS DATA"}			
+			$scope.MidasErrorMsg = "ERROR RETRIEVING MIDAS DATA"	;
+			OnError('ERROR RETRIEVING MIDAS DATA');			
+			}
             console.log("ERROR IN MIDAS TAB:", msg);
         });
     }
@@ -136,12 +210,18 @@ app.controller("workbookcontroller", function ($scope, Service) {
             if($scope.FFMS.length < 1){
 				//console.log('NO DATA FOUND');
                 $scope.noFfmDataFlag = true;
+				OnError('NO DATA FOUND');
             }
+			else
+				HideLoader();
+			
         }, function (msg) {     
 			if(msg.status > 1 || msg.status== -1){
 			$scope.FfmErrorFlag = true;	
 			$scope.ShowLoadingMsg = false;
-			$scope.FfmErrorMsg = "ERROR RETRIEVING FFM DATA"}			
+			$scope.FfmErrorMsg = "ERROR RETRIEVING FFM DATA";
+			OnError('ERROR RETRIEVING FFM DATA');			
+			}
             console.log("ERROR IN FFM TAB:", msg);
         });
     }
@@ -157,17 +237,24 @@ app.controller("workbookcontroller", function ($scope, Service) {
             if($scope.RCNO.length < 1){
 				//console.log('NO DATA FOUND');
                 $scope.noRcnoDataFlag = true;
+				OnError('NO DATA FOUND');
             }
+			else
+				HideLoader();
 }, function (msg) { 	
 			if(msg.status > 1 || msg.status== -1){
-			$scope.RcnoErrorFlag = true;
-			$scope.ShowLoadingMsg = false;			
-			$scope.RcnoErrorMsg = "ERROR RETRIEVING RCNO DATA"}			
+				$scope.RcnoErrorFlag = true;
+				$scope.ShowLoadingMsg = false;			
+				$scope.RcnoErrorMsg = "ERROR RETRIEVING RCNO DATA";
+				OnError('ERROR RETRIEVING RCNO DATA');				
+			}				
             console.log("ERROR IN RCNO TAB:", msg);
         });
     }
 
     $scope.fn_SetTablename = function (TableNo) {
+		ShowLoader();
+		//OnError('text here')
         if (TableNo == 1) {
             $scope.TableName = "MIDAS";
         }
